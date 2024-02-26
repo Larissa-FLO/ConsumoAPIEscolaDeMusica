fb489c7ff93cb2a21446919d9689ac61d6f2d277
Larissa Lopes de Oliveira <larissa.arqueo@outlook.com>
2024-02-26 16:50:35 -0300

    consumo de api

--- assets/script.js
+++ assets/script.js
@@ -5,4 +5,14 @@ function clicarBotao(){
 
 async function buscarAluno(aluno){
     let dadosAluno = await fetch(`https://localhost:7236/api/Alunos/${aluno}`).then(Response => Response.json());
+    console.log(dadosAluno);
+    dadosTela(dadosAluno)
 }
+
+function dadosTela(dadosAluno){
+    document.getElementById('nome').value = dadosAluno.nomeAluno;
+    document.getElementById('dataNascimento').value = dadosAluno.dataNasc;
+    document.getElementById('dataInicio').value = dadosAluno.dataNasc;
+    document.getElementById('turma').value = dadosAluno.codTurma;
+}
+
