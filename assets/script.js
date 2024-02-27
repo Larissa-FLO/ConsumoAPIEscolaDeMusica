4711dd226a4fa2c2b2548ec596b8de87c51c058a
Larissa Lopes de Oliveira <larissa.arqueo@outlook.com>
2024-02-27 01:00:06 -0300

    Consumo de API e estilização

--- assets/script.js
+++ assets/script.js
@@ -14,5 +14,29 @@ function dadosTela(dadosAluno){
     document.getElementById('dataNascimento').value = dadosAluno.dataNasc;
     document.getElementById('dataInicio').value = dadosAluno.dataNasc;
     document.getElementById('turma').value = dadosAluno.codTurma;
+    buscarTurma();
 }
 
+async function buscarTurma(){
+    let turma = document.getElementById("turma").value;
+    let dadosTurma = await fetch(`https://localhost:7236/api/Turmas/${turma}`).then(Response => Response.json());
+    console.log(dadosTurma);
+    mostrarDadosTurma(dadosTurma);
+}
+
+function mostrarDadosTurma(dadosTurma){
+    document.getElementById('professor').value = dadosTurma.profResponsavel;
+    document.getElementById('instrumento').value = dadosTurma.instrumento;
+    document.getElementById('turno').value = dadosTurma.turno;
+}
+
+function limparDados(){
+    document.getElementById("idAluno").value = "";
+    document.getElementById("nome").value = "";
+    document.getElementById("dataNascimento").value = "";
+    document.getElementById("dataInicio").value = "";
+    document.getElementById("turma").value = "";
+    document.getElementById("professor").value = "";
+    document.getElementById("instrumento").value = "";
+    document.getElementById("turno").value = "";
+}
